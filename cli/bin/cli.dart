36c8a834d253da2ba6e1b1fe4827870d86047d37
Dima Zotov <dima35zotov@mail.ru>
2026-05-19 19:48:36 +0300

    Модифицировали функцию searchWikipedia добавив в ней вызов функции getWikipediaArticle и вывод полученного результата после запроса к api

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -18,7 +18,6 @@ void main(List<String> arguments) {
 
 void searchWikipedia(List<String>? arguments) async {
   final String articleTitle;
-
   if (arguments == null || arguments.isEmpty) {
     print('Please provide an article title.');
     final inputFromStdin = stdin.readLineSync();
@@ -32,8 +31,10 @@ void searchWikipedia(List<String>? arguments) async {
   }
 
   print('Looking up articles about "$articleTitle". Please wait.');
-  print('Here ya go!');
-  print('(Pretend this is an article about "$articleTitle")');
+
+  // Call the API and await the result
+  var articleContent = await getWikipediaArticle(articleTitle);
+  print(articleContent);
 }
 
 
